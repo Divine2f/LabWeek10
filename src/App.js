cf74018f2894f22e95498fd05183a52b9e6e1800
Divine2f <divine2f@gmail.com>
2023-11-13 00:13:15 -0500

    LabWeek10

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Student from './Student';
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+      <h1>Data Entry Form</h1>
+      <Student/>
+      </div>
+    </>
+  );
+}
+
+export default App;
